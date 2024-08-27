728dc2ccf9ed47329b1e0bbdfe924be506e18510
Ken Yamaji <ken.yamaji@gmail.com>
2024-08-27 19:51:18 +0900

    20240827

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -5,15 +5,15 @@ import os
 
 def generate_launch_description():
     pkg_share = launch_ros.substitutions.FindPackageShare(package='robot_description').find('robot_description')
-    default_model_path = os.path.join(pkg_share, 'src/description/robot_description.urdf')
-    #default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
+    # default_model_path = os.path.join(pkg_share, 'src/description/robot_description.urdf')
+    # default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
     default_rviz_config_path = os.path.join(pkg_share, 'rviz/nav2_default_view.rviz')
 
-    robot_state_publisher_node = launch_ros.actions.Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
-    )
+    # robot_state_publisher_node = launch_ros.actions.Node(
+    #     package='robot_state_publisher',
+    #     executable='robot_state_publisher',
+    #     parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
+    # )
     # joint_state_publisher_node = launch_ros.actions.Node(
     #     package='joint_state_publisher',
     #     executable='joint_state_publisher',
@@ -35,26 +35,26 @@ def generate_launch_description():
         arguments=['-d', LaunchConfiguration('rvizconfig')],
     )
 
-    robot_localization_node = launch_ros.actions.Node(
-        package='robot_localization',
-        executable='ekf_node',
-        name='ekf_filter_node',
-        output='screen',
-        parameters=[os.path.join(pkg_share, 'config/ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}]
-    )
+    # robot_localization_node = launch_ros.actions.Node(
+    #     package='robot_localization',
+    #     executable='ekf_node',
+    #     name='ekf_filter_node',
+    #     output='screen',
+    #     parameters=[os.path.join(pkg_share, 'config/ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}]
+    # )
 
     return launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
-                                            description='Flag to enable joint_state_publisher_gui'),
-        launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
-                                            description='Absolute path to robot urdf file'),
+        # launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
+        #                                     description='Flag to enable joint_state_publisher_gui'),
+        # launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
+        #                                     description='Absolute path to robot urdf file'),
         launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
                                             description='Absolute path to rviz config file'),
-        launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='True',
-                                            description='Flag to enable use_sim_time'),
+        # launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='True',
+        #                                     description='Flag to enable use_sim_time'),
         # joint_state_publisher_node,
         # joint_state_publisher_gui_node,
-        robot_state_publisher_node,
-        robot_localization_node,
+        # robot_state_publisher_node,
+        # robot_localization_node,
         rviz_node
     ])
