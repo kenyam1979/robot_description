aa765c307a534ce40f0b96de0157649af7bfaa22
kenyam1979 <ken.yamaji@gmail.com>
2025-01-25 16:45:59 +0900

    20250125

--- launch/async_slam.launch.py
+++ launch/async_slam.launch.py
@@ -10,12 +10,20 @@ def generate_launch_description():
     pkg_share = launch_ros.substitutions.FindPackageShare(package='robot_description').find('robot_description')
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     slam_params = os.path.join(pkg_share, "config", 'async_slam_settings.yaml')
-    
+
+    slam_params_file = LaunchConfiguration('slam_params_file')
+    declare_slam_params_file_cmd = launch.actions.DeclareLaunchArgument(
+        'slam_params_file',
+        default_value=slam_params,
+        description='Full path to the ROS2 parameters file to use for the slam_toolbox node')
+
+    print(slam_params)
+ 
     return launch.LaunchDescription([
         Node(
             parameters=[
-            {"slam_params_file": slam_params},
-            {'use_sim_time': use_sim_time}
+                slam_params_file,
+                {'use_sim_time': use_sim_time}
             ],
             package='slam_toolbox',
             executable='async_slam_toolbox_node',
